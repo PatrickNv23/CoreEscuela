30084eacaeb9d0de090c319de12b35d271ca3669
Patrick Navarro <patricknv230101@gmail.com>
2022-12-15 15:23:38 -0500

    Escuela parte 1 creación

---
+++ CoreEscuela/Entidades/Escuela.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.Entidades
+{
+    internal class Escuela
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int AñoDeCreación { get; set; }
+        public String Pais { get; set; }
+
+        public String Ciudad { get; set; }
+
+        public TipoEscuela TipoEscuela { get; set; }
+
+
+        public Escuela(String nombre, int año, String pais)
+        {
+            this.nombre = nombre;
+            this.AñoDeCreación = año;
+            this.Pais = pais;
+        }
+
+        public Escuela(String nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
+
+        public Escuela(String nombre, int año, TipoEscuela tipo, String pais = "", String ciudad = "")
+        {
+            (Nombre, AñoDeCreación) = (nombre, año);
+            TipoEscuela = tipo;
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad: {Ciudad}";
+        }
+
+    }
+}
---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogotá";
+            escuela.TipoEscuela = TipoEscuela.Primaria;
+            Console.WriteLine(escuela);
+            var escuela2 = new Escuela("Escuela 2", 2013, TipoEscuela.Secundaria, pais:"Mexico");
+            Console.WriteLine(escuela2);
+        }
+    }
+}
